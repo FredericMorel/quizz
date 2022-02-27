19cbe0777562b03fec9b21cb8c19a71222b67b35
Morel Frédéric <frederic.morel.ars@outlook.fr>
2022-02-27 18:16:46 +0400

    fin de l'exo

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
-import 'quizBrian.dart';
+import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 QuizBrain qb = QuizBrain();
 
 void main() {
-  runApp(const QuizzApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(child: QuizzApp()),
+      ),
+    ),
+  );
 }
 
 class QuizzApp extends StatefulWidget {
@@ -16,90 +24,95 @@ class QuizzApp extends StatefulWidget {
 
 class _QuizzAppState extends State<QuizzApp> {
   List<Icon> suiviScore = [];
+  int score = 0;
 
   checkAnswer(bool x) {
     bool bonnereponse = qb.getQuestionReponse();
     setState(() {
       //if (suiviScore.length != qb.)
       if (bonnereponse == x) {
-        suiviScore.add(Icon(Icons.close, color: Colors.red));
+        score++;
+        suiviScore.add(Icon(Icons.close, color: Colors.green));
       } else {
-        suiviScore.add(Icon(Icons.check, color: Colors.green));
+        suiviScore.add(Icon(Icons.check, color: Colors.red));
       }
-
-      qb.nextQuestion();
+      qb.nextQuestion(); // questionNumber = question number + 1
     });
+    if (suiviScore.length == qb.nbQuestion()) {
+      
+      Alert(
+              context: context,
+              title: "Fin du quiz",
+              desc: "Vous avez eu $score bonnes réponses.")
+          .show();
+      score = 0;
+      qb.reset();
+      suiviScore = [];
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
-        body: SafeArea(
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                /* Texte principal */
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          /* Texte principal */
 
-                Expanded(
-                  flex: 5,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Center(
-                      child: Text(
-                        qb.getQuestionEnonce(),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: Colors.white, fontSize: 25.0),
-                      ),
-                    ),
-                  ),
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Center(
+                child: Text(
+                  qb.getQuestionEnonce(),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 25.0),
                 ),
+              ),
+            ),
+          ),
 
-                /* --------------- Bouton vrai ---------------------*/
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: TextButton(
-                      style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.green)),
-                      onPressed: () {
-                        checkAnswer(true);
-                      },
-                      child: Text(
-                        "Vrai",
-                        style: TextStyle(color: Colors.white, fontSize: 20.0),
-                      ),
-                    ),
-                  ),
+          /* --------------- Bouton vrai ---------------------*/
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: TextButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green)),
+                onPressed: () {
+                  checkAnswer(true);
+                },
+                child: Text(
+                  "Vrai",
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
                 ),
+              ),
+            ),
+          ),
 
-                /* ----------------------  Bouton Faux --------------------------*/
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: TextButton(
-                      style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.red)),
-                      onPressed: () {
-                        checkAnswer(false);
-                      },
-                      child: Text(
-                        "Faux",
-                        style: TextStyle(color: Colors.white, fontSize: 20.0),
-                      ),
-                    ),
-                  ),
+          /* ----------------------  Bouton Faux --------------------------*/
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: TextButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.red)),
+                onPressed: () {
+                  checkAnswer(false);
+                },
+                child: Text(
+                  "Faux",
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
                 ),
-                Row(
-                  children: suiviScore,
-                )
-              ]),
-        ),
-      ),
-    );
+              ),
+            ),
+          ),
+          Row(
+            children: suiviScore,
+          )
+        ]);
   }
 }
