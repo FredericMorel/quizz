4fdd43b5c6b8d218f429caaca991f69219843f29
Morel Frédéric <frederic.morel.ars@outlook.fr>
2022-02-27 18:15:28 +0400

    fin de l'exercice

---
+++ lib/quiz_brain.dart
@@ -0,0 +1,41 @@
+import 'question.dart';
+
+class QuizBrain {
+  final List<Question> _questions = [
+    Question("Le piton des neiges est un volcan de la Réunion", true),
+    Question("Flutter permet de faire des applications web également", true),
+    Question("PHP est le langage utilisé par flutter", false),
+    Question("The end", true)
+  ];
+  int _questionNumber = 0;
+
+  String getQuestionEnonce() {
+    return _questions[_questionNumber].enonce;
+  }
+
+  bool getQuestionReponse() {
+    return _questions[_questionNumber].reponse;
+  }
+
+  void nextQuestion() {
+    if (_questionNumber < _questions.length - 1) {
+      _questionNumber++;
+    }
+  }
+
+  bool fini() {
+    if (_questionNumber >= _questions.length - 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  int nbQuestion() {
+    return _questions.length;
+  }
+
+  void reset() {
+    _questionNumber = 0;
+  }
+}
